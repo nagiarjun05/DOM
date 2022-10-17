f8bc1c0d108b77e48f110476de9ed776d2937045
nagiarjun05 <nagiarjun05@gmail.com>
2022-10-18 01:16:14 +0530

    querySelectorAndQuerySelectorAll added

--- dom.js
+++ dom.js
@@ -13,11 +13,11 @@ console.log(document.title);
 document.title='Hello';*/
 
 //Get Element By ID
-console.log(document.getElementById('header-title'));
+//console.log(document.getElementById('header-title'));
 //var div1=document.getElementById('main');
 //div1.textContent='Hello Arjun';
 
-var header=document.getElementById('main-header');
+/*var header=document.getElementById('main-header');
 header.style.border='solid 3px #000';
 
 var addlister=document.getElementById('main');
@@ -25,7 +25,7 @@ addlister.children[0].style.fontWeight='bold';
 addlister.children[0].style.color='green';
 
 console.log(addlister);
-console.log(addlister.firstElementChild);
+console.log(addlister.firstElementChild);*/
 
 /*var items=document.getElementsByClassName('list-group-item');
 console.log(items);
@@ -36,9 +36,25 @@ for(let i=0;i<items.length;i++){
     items[i].style.fontWeight='bold';
 }*/
 
-var li=document.getElementsByTagName('li');
+/*var li=document.getElementsByTagName('li');
 console.log(li);
 
 for(let i=0;i<li.length;i++){
     li[i].style.fontWeight='bold';
+}*/
+
+/*var secondElement=document.querySelector('.list-group-item:nth-child(2)');
+secondElement.style.backgroundColor='green';
+
+var thirdElement=document.querySelector('.list-group-item:nth-child(3)');
+thirdElement.style.display='none';*/
+
+var items=document.querySelectorAll('.list-group-item');
+
+items[1].style.color='green';
+
+var oddItems=document.querySelectorAll('.list-group-item:nth-child(odd)');
+
+for(let i=0;i<oddItems.length;i++){
+    oddItems[i].style.backgroundColor='green';      
 }
