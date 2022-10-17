27020240a2989950657f67e44d762041968530c3
nagiarjun05 <nagiarjun05@gmail.com>
2022-10-18 00:29:35 +0530

    GetElementByClassName added

--- dom.js
+++ dom.js
@@ -26,3 +26,12 @@ addlister.children[0].style.color='green';
 
 console.log(addlister);
 console.log(addlister.firstElementChild);
+
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+
+items[2].style.backgroundColor='green';
+
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
