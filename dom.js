3aaa05a6d87ed8ab26877c1a2b6c1f264666721d
nagiarjun05 <nagiarjun05@gmail.com>
2022-10-18 00:45:45 +0530

    GetElementbyTagandClassName added

--- dom.js
+++ dom.js
@@ -27,11 +27,18 @@ addlister.children[0].style.color='green';
 console.log(addlister);
 console.log(addlister.firstElementChild);
 
-var items=document.getElementsByClassName('list-group-item');
+/*var items=document.getElementsByClassName('list-group-item');
 console.log(items);
 
 items[2].style.backgroundColor='green';
 
 for(let i=0;i<items.length;i++){
     items[i].style.fontWeight='bold';
+}*/
+
+var li=document.getElementsByTagName('li');
+console.log(li);
+
+for(let i=0;i<li.length;i++){
+    li[i].style.fontWeight='bold';
 }
